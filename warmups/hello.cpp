3d09c50e94d8f5afd983a012e7997c3d883ca33c
Andrea Pagotto <andreapagotto@Jacks-iMac.local>
2018-10-07 13:16:31 -0400

    removing executables

---
+++ warmups/hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+// Main algorithm: Use modulo to figure out which indices we need to print without actually
+// rotating the array.
+int main() {
+	// Read input
+	cout << "Hello World" << endl;
+    return 0;
+}
